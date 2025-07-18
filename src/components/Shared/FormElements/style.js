19bb673680cf031a8d8a6c05e693b60050eb6520
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-07-18 16:28:12 +0530

    added sign up form

--- src/components/Shared/FormElements/style.js
+++ src/components/Shared/FormElements/style.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
-import { textColor } from "../../../styles/mixins";
+import {
+  textColor,
+  fontFamily,
+  backgroundColor,
+  borderStyleColor,
+} from "../../../styles/mixins";
 
 export const AppFromField = styled.div`
   width: 100%;
@@ -10,6 +15,43 @@ export const AppFromField = styled.div`
   label {
     font-size: 15px;
     ${textColor("bodytext")};
+  }
+`;
+
+export const AppPhoneCover = styled.div`
+  display: flex;
+  gap: 10px;
+
+  & > select {
+    ${fontFamily};
+    width: 30%;
+    height: 44px;
+    padding: 0 10px;
+    border-radius: 6px;
+    font-weight: 500;
+    font-size: 14px;
+    ${borderStyleColor(1, "tableborder")}
+
+    &.disabled {
+      ${backgroundColor("gray")};
+      cursor: not-allowed;
+    }
+  }
+
+  & > input {
+    ${fontFamily};
+    width: 70%;
+    height: 44px;
+    padding: 0 16px;
+    border-radius: 6px;
+    font-weight: 500;
+    font-size: 15px;
+    ${borderStyleColor(1, "inputborder")}
+
+    &.disabled {
+      ${backgroundColor("gray")};
+      cursor: not-allowed;
+    }
   }
 `;
 
