1de29a70fabf289629ff45ffab3c599f5cad56c8
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-07-19 18:56:32 +0530

    added all the pages

---
+++ src/Pages/Main/Admission/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AdmissionPage = () => {
+  return <div>AdmissionPage</div>;
+};
---
+++ src/Pages/Main/HumanResources/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HumanResourcesPage = () => {
+  return <div>HumanResourcesPage</div>;
+};
