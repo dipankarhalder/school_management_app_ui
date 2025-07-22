63aa3752bd32ee64ff3209214df60b385c904b77
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-07-23 05:11:54 +0530

    added calender

--- src/Pages/Main/Schedules/index.jsx
+++ src/Pages/Main/Schedules/index.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import { TopBar } from "../../../components/Main/TopBar";
+import { locationInfo } from "../../../Constant";
+import { Calendar } from "../../../Shared/Calendar";
+import { AppMainLayoutCover, AppTableDataInformation } from "../style";
 
 export const SchedulesPage = () => {
-  return <div>SchedulesPage</div>;
+  return (
+    <AppMainLayoutCover>
+      <TopBar location={locationInfo} />
+      <AppTableDataInformation>
+        <Calendar />
+      </AppTableDataInformation>
+    </AppMainLayoutCover>
+  );
 };
