5b51f8c8fc0d76eb46c442d71847d5073921a0c9
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-07-24 01:15:06 +0530

    added calender view

--- src/Pages/Main/style.js
+++ src/Pages/Main/style.js
@@ -12,3 +12,29 @@ export const AppTableDataInformation = styled.div`
   width: 100%;
   padding: 16px 24px 0;
 `;
+
+export const ApplicationCoverContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  margin-bottom: 16px;
+`;
+
+export const AppMainPageHeading = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  & > h1 {
+    font-size: 18px;
+    font-weight: 600;
+  }
+`;
+
+export const AppContentDiv = styled.div`
+  display: flex;
+  width: 100%;
+  margin-top: 14px;
+  padding-top: 12px;
+  border-top: 1px solid #d2dbe5;
+`;
